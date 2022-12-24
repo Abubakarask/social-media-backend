8d7f91f16c76603eae23a09bde3c5177b946511a
Amaan2210 <amaan@naya.studio>
2022-12-24 23:43:33 +0530

    Updated Cors Policy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 const path = require("path");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
@@ -10,6 +11,15 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [
+      /localhost:[0-9]+$/,
+    ],
+    credentials: true,
+  }),
+);
 
 // Importing Routes
 const post = require("./routes/post");
